ca33ea5ca57f37dfcd429606aa444c5cd78dddbc
rnphilp <rnphilp@users.noreply.github.com>
2019-04-23 17:07:20 +0100

    adds app state and high level routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import MainSite from './components/MainSite';
+import { Router } from '@reach/router';
+import LandingPage from './components/LandingPage';
 
-function App() {
-  return <div className="App" />;
+class App extends Component {
+  state = {
+    loggedIn: true,
+    username: 'weegembump',
+    drawerOpen: false,
+    topics: [
+      {
+        slug: 'coding',
+        description: 'Code is love, code is life'
+      },
+      {
+        slug: 'football',
+        description: 'FOOTIE!'
+      },
+      {
+        slug: 'cooking',
+        description: 'Hey good looking, what you got cooking?'
+      }
+    ]
+  };
+
+  render() {
+    const { drawerOpen } = this.state;
+    return (
+      <div className="App">
+        <Router>
+          <LandingPage path="/" />
+          <MainSite path="/*" drawerOpen={drawerOpen} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
