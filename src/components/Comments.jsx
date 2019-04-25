0d44aa16f4915319baf1ec9c1c4b54c8daad4e28
rnphilp <rnphilp@users.noreply.github.com>
2019-04-25 12:59:42 +0100

    adds sort functionality to comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Typography, Paper } from '@material-ui/core';
-import * as api from './api';
+import * as api from '../api';
+import { withStyles } from '@material-ui/core/styles';
+import Sort from './Sort';
 
 class Comments extends Component {
   state = {
-    comments: []
+    comments: [],
+    sortBy: 'created_at',
+    sortAsc: false
   };
 
   render() {
     const { commentCount } = this.props;
-    const { comments } = this.state;
+    const { comments, sortBy, sortAsc } = this.state;
+    const sortByOptions = [
+      { display: 'date', name: 'created_at' },
+      { display: 'author', name: 'author' },
+      { display: 'votes', name: 'votes' }
+    ];
     return (
       <div>
         <Typography variant="h6">
           {`${commentCount} Comment${commentCount > 1 && 's'}`}
         </Typography>
+        <Sort
+          sortByOptions={sortByOptions}
+          sortBy={sortBy}
+          sortAsc={sortAsc}
+          handleChange={this.handleChange}
+          toggleSortOrder={this.toggleSortOrder}
+        />
         {comments.map(comment => {
           return (
             <Paper key={comment.comment_id}>
@@ -34,12 +50,34 @@ class Comments extends Component {
     this.getComments();
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    const { articleId } = this.props;
+    if (prevState.sortBy !== this.state.sortBy) this.getComments(articleId);
+    if (prevState.sortAsc !== this.state.sortAsc) this.getComments(articleId);
+  }
+
   getComments = () => {
-    api.getComments(this.props.articleId).then(comments => {
+    const { sortBy, sortAsc } = this.state;
+    const queries = {
+      sort_by: sortBy,
+      order: sortAsc ? 'asc' : 'desc'
+    };
+    console.log(queries);
+    api.getComments(this.props.articleId, queries).then(comments => {
       this.setState({
         comments
       });
     });
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  toggleSortOrder = () => {
+    this.setState(state => ({
+      sortAsc: !state.sortAsc
+    }));
   };
 }
 
@@ -48,4 +86,4 @@ Comments.propTypes = {
   articleId: PropTypes.number.isRequired
 };
 
-export default Comments;
+export default withStyles({})(Comments);
