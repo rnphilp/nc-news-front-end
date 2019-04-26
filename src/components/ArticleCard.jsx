c49dc25b235db3178f9734dc22cb7b1f75157b50
rnphilp <rnphilp@users.noreply.github.com>
2019-04-26 11:41:34 +0100

    adds syling to article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Paper, Typography, Link, Chip, Avatar } from '@material-ui/core';
+import { Paper, Typography, Link, Chip, Badge } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import {
+  HowToVoteRounded as VotesIcon,
+  CommentRounded as CommentIcon
+} from '@material-ui/icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSlackHash } from '@fortawesome/free-brands-svg-icons';
 
@@ -9,30 +13,55 @@ import { Link as ReachLink } from '@reach/router';
 
 const styles = theme => ({
   root: {
-    margin: '10px'
+    margin: theme.spacing.unit * 2
+  },
+  Paper: {
+    padding: theme.spacing.unit * 3
+  },
+  Chip: {
+    marginTop: theme.spacing.unit * 1,
+    marginBottom: theme.spacing.unit * 1
+  },
+  margin: {
+    margin: theme.spacing.unit * 2
   }
 });
 
 const ArticleCard = props => {
   const { article, classes, navigate } = props;
+  console.log(article);
   return (
     <div className={classes.root}>
-      <Paper>
+      <Paper className={classes.Paper}>
         <Link component={ReachLink} to={`${article.article_id}`}>
           <Typography button="true" variant="h5" component="h3">
             {article.title}
           </Typography>
         </Link>
-        <Typography component="p">{article.author}</Typography>
-        <Typography component="p">{article.created_at}</Typography>
         <Chip
           icon={<FontAwesomeIcon icon={faSlackHash} />}
           label={article.topic}
           onClick={() => navigate(`/articles/?topic=${article.topic}`)}
-          className={classes.chip}
+          className={classes.Chip}
         />
-        <Typography component="p">Votes: {article.votes}</Typography>
-        <Typography component="p">Comments: {article.comment_count}</Typography>
+        <Typography component="p">{article.author}</Typography>
+        <Typography component="p">{article.created_at}</Typography>
+        <Badge
+          className={classes.margin}
+          badgeContent={article.votes}
+          color="primary"
+          showZero
+        >
+          <VotesIcon />
+        </Badge>
+        <Badge
+          className={classes.margin}
+          badgeContent={article.comment_count}
+          color="primary"
+          showZero
+        >
+          <CommentIcon />
+        </Badge>
       </Paper>
     </div>
   );
