6ae2ade24e97f297c21447445f932728feeda04e
rnphilp <rnphilp@users.noreply.github.com>
2019-04-26 10:29:51 +0100

    transition added to mainSite on drawer change

--- src/components/MainSite.jsx
+++ src/components/MainSite.jsx
@@ -5,15 +5,24 @@ import { Router } from '@reach/router';
 import Articles from './Articles';
 import { withStyles } from '@material-ui/core/styles';
 import Article from './Article';
+import classNames from 'classnames';
 
 const styles = theme => ({
-  root: {
-    display: 'flex'
-  },
-  content: {
+  main: {
     flexGrow: 1,
     padding: theme.spacing.unit * 3,
-    'padding-top': '70px'
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: 0
+  },
+  mainShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: theme.drawer.width
   }
 });
 class MainSite extends Component {
@@ -31,8 +40,15 @@ class MainSite extends Component {
           title={title}
           openLogin={openLogin}
           logout={logout}
+          className={classNames(classes.AppBar, {
+            [classes.AppBarShift]: drawerOpen
+          })}
         />
-        <main className={classes.content}>
+        <main
+          className={classNames(classes.main, {
+            [classes.mainShift]: drawerOpen
+          })}
+        >
           <Router>
             <Articles path="/articles" />
             <Article path="/articles/:articleId" />
